f1505a0b5b125ee4e52f97c9db982839b19e0e81
hujun3 <hujun3@xiaomi.com>
2022-11-15 09:58:26 +0800

    解决nacos注册多协议问题

--- dubbo-registry/dubbo-registry-nacos/src/main/java/org/apache/dubbo/registry/nacos/NacosRegistryFactory.java
+++ dubbo-registry/dubbo-registry-nacos/src/main/java/org/apache/dubbo/registry/nacos/NacosRegistryFactory.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.dubbo.registry.nacos;
 
 import org.apache.dubbo.common.URL;
@@ -25,6 +26,7 @@ import org.apache.dubbo.registry.support.AbstractRegistryFactory;
 
 import static org.apache.dubbo.common.constants.CommonConstants.CONFIG_NAMESPACE_KEY;
 import static org.apache.dubbo.registry.nacos.util.NacosNamingServiceUtils.createNamingService;
+import static org.apache.dubbo.rpc.cluster.Constants.EXPORT_KEY;
 
 /**
  * Nacos {@link RegistryFactory}
@@ -32,18 +34,33 @@ import static org.apache.dubbo.registry.nacos.util.NacosNamingServiceUtils.creat
  * @since 2.6.5
  */
 public class NacosRegistryFactory extends AbstractRegistryFactory {
-
+    
+    @Override
+    public Registry getRegistry(URL url) {
+        URL providerUrl = getProviderUrl(url);
+        if (providerUrl != null) {
+            String port = providerUrl.getParameter("bind.port");
+            url = url.addParameter(CommonConstants.PROTOCOL_KEY, providerUrl.getProtocol());
+            url = url.addParameter(CommonConstants.PORT_KEY, port);
+        }
+        return super.getRegistry(url);
+    }
+    
     @Override
     protected String createRegistryCacheKey(URL url) {
         String namespace = url.getParameter(CONFIG_NAMESPACE_KEY);
+        String protocol = url.getParameter(CommonConstants.PROTOCOL_KEY);
+        String port = url.getParameter(CommonConstants.PORT_KEY);
         url = URL.valueOf(url.toServiceStringWithoutResolving());
         if (StringUtils.isNotEmpty(namespace) && !CommonConstants.DUBBO.equals(namespace)) {
             // ignore "dubbo" namespace, make the behavior equivalent to configcenter
             url = url.addParameter(CONFIG_NAMESPACE_KEY, namespace);
         }
+        url = url.addParameter(CommonConstants.PROTOCOL_KEY, protocol);
+        url = url.addParameter(CommonConstants.PORT_KEY, port);
         return url.toFullString();
     }
-
+    
     @Override
     protected Registry createRegistry(URL url) {
         URL nacosURL = url;
@@ -52,5 +69,13 @@ public class NacosRegistryFactory extends AbstractRegistryFactory {
             nacosURL = url.removeParameter(CONFIG_NAMESPACE_KEY);
         }
         return new NacosRegistry(nacosURL, createNamingService(nacosURL));
+    }
+    
+    private URL getProviderUrl(URL url) {
+        String export = url.getParameterAndDecoded(EXPORT_KEY);
+        if (export == null || export.length() == 0) {
+            return null;
+        }
+        return URL.valueOf(export);
     }
 }
